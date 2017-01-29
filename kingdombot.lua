0239fb55f2b16e22066fbe485f7941dacc0a0a72
GustavoBlaze <gustavinho__vr@hotmail.com>
2017-01-29 03:23:48 -0200

    Update master

--- kingdombot.lua
+++ kingdombot.lua
@@ -6,7 +6,7 @@ g_window = nil
 optionsButton = {}
 
 function init()
-    g_button = modules.client_topmenu.addRightButton('kingdom_button', tr('Kingdom Bot'), 'icon.png', toggle, true)
+    g_button = modules.client_topmenu.addRightToggleButton('kingdom_button', tr('Kingdom Bot'), 'images/bot_icon.png', toggle, true)
     g_window = g_ui.displayUI("kingdombot.otui")
     g_window:hide()
     
@@ -24,9 +24,11 @@ function toggle()
     
     if g_window:isVisible() then
         g_window:hide()
+        g_button:setOn(false)
     else
         g_window:show()
         g_window:focus(true)
+        g_button:setOn(true)
     end
 end
 
